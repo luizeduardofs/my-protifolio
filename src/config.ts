37d75b76ba696555bbc63af34040f26281b7036b
Luiz Eduardo F S <edu.dev.py@gmail.com>
2024-04-05 19:45:24 -0300

    Fix site description and image thumbnail

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Luiz Eduardo | Portfólio Pessoal';
+export const SITE_DESCRIPTION = 'Desenvolvedor especializado em Flutter e FlutterFlow, criando experiências digitais excepcionais. Explore meu portfólio para descobrir projetos inovadores e funcionais que combinam design intuitivo com tecnologia de ponta.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
